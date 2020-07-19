482e8177f5851e690b86ef1aa05fdf8711c0fcab
vike <54340686+sdhushu@users.noreply.github.com>
2020-07-19 11:36:04 +0800

    Add files via upload

---
+++ pygui/Spider/baiduSpider/bilbilSpider/bilbilSpider.py
@@ -0,0 +1,123 @@
+import time
+
+from PIL import Image
+from pyquery import PyQuery as pq
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+'''
+本程序是爬取B站个人数据
+主入口为star_page.py
+'''
+
+result = []
+sum_result = []
+
+#将爬虫程序封装成类
+class BilBil:
+    #初始化属性
+    def __init__(self):
+        self.url = 'https://passport.bilibili.com/login'
+        self.title = []
+        self.watch_time = []
+        self.type = []
+        self.username = []
+        chrome_options = Options()
+        chrome_options.add_argument('--disable-gpu')  # 谷歌文档提到需要加上这个属性来规避bug
+        chrome_options.add_argument('--headless')  # 浏览器不提供可视化页面.
+        browser = webdriver.Chrome(options=chrome_options)
+        # browser.maximize_window()
+        self.login(browser)
+
+    #登录
+    def login(self,browser):
+        browser.get(self.url)
+        time.sleep(2)
+        browser.save_screenshot('login.png')
+        img = Image.open('login.png')
+        box = (150, 260, 350, 460)
+        img = img.crop(box)
+        img.save('captcha.png')
+        img.show()
+        self.data(browser)
+        self.user_info(browser)
+
+    #拉取数据
+    def data(self,browser):
+        time.sleep(1)
+        browser.find_element_by_xpath('//*[@id="internationalHeader"]/div[1]/div/div[3]/div[2]/div[6]/span/div[2]/span/span').click()
+        handle = browser.window_handles
+        browser.switch_to.window(handle[-1])
+        time.sleep(1)
+        for i in range(4):
+            browser.execute_script("var action=document.documentElement.scrollTop=60000")
+            time.sleep(2)
+        doc =pq(browser.page_source)
+        title = doc('.title').items()
+        watch_time = doc('.lastplay-t').items()
+        video_type = doc('.name').items()
+        username = doc('span.username').items()
+        for i in title:
+            self.title.append(i.text())
+        for x in watch_time:
+            self.watch_time.append(x.text())
+        for s in video_type:
+            self.type.append(s.text())
+        for z in username:
+            self.username.append(z.text())
+        print(self.username)
+        print('------------')
+        print(self.type[122:])
+        print(type(self.type[122:]))
+        dict1 = {}
+        for i in self.type[122:]:
+            if i not in dict1.keys():
+                dict1[i] = self.type[122:].count(i)
+        name =max(dict1,key=dict1.get)
+
+        dict2  ={}
+        for x in self.username:
+            if x not in dict2.keys():
+                dict2[x] = self.username.count(x)
+            user1 = max(dict2, key=dict2.get)
+        print(f'视频名{self.title}')
+        print(f'观看时间{self.watch_time}')
+        print(f'视频类型{self.type[122:]}')
+        print(f'您最后一次登录时间为{self.watch_time[0]}')
+        print(f'您最喜欢观看的视频系列为: {name}')
+        print(f'您最喜欢的博主为: {user1}')
+        sum_result.append(self.watch_time)
+        sum_result.append(self.title)
+        sum_result.append(self.type[122:])
+        result.append(self.watch_time[0])
+        result.append(name)
+        result.append(user1)
+
+
+    #拿取用户信息
+    def user_info(self,browser):
+        browser.get('https://space.bilibili.com/')
+        time.sleep(1)
+        handle = browser.window_handles
+        browser.switch_to.window(handle[-1])
+        browser.save_screenshot('user.png')
+        doc = pq(browser.page_source)
+        username = doc('#h-name').text()
+        word = doc('.h-sign').attr('title')
+        print(f'用户名为:{username}')
+        sum_result.append(username)
+        result.append(username)
+        sum_result.append(word)
+        print(f'个性签名为:{word}')
+        result.append(word)
+        browser.quit()
+
+
+
+
+
+#将参数传到gui使用
+def bilibili_go():
+    BilBil()
+    result.append(sum_result)
+    return result
---
+++ pygui/Spider/baiduSpider/studySpider/studySpider.py
@@ -0,0 +1,119 @@
+import requests
+
+from pyquery import PyQuery as pq
+from aip import AipOcr
+from PIL import ImageFilter
+from PIL import Image
+
+'''
+本程序使用了js渗透 模拟登录
+主入口star_page
+'''
+
+#将程序封装成类 方便调用
+class Login(object):
+    def __init__(self):
+        #初始化属性
+        self.headers = {
+            'Referer':'https://account.chsi.com.cn/passport/login?service=https%3A%2F%2Fmy.chsi.com.cn%2Farchive%2Fj_spring_cas_security_check',
+            'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',
+            'Host':'account.chsi.com.cn'
+        }
+        self.header = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',
+        }
+        self.prarms = {
+            'trnd': 1593923253028,
+        }
+        self.APP_ID = '21139008'
+        self.API_KEY = '9S5htfoQdGKfn2wI21R1G5jn'
+        self.SECRET_KEY = 'pbGVKG13CDrp4WLhwmOHV6XIv2D3XyYh'
+        self.login_url = 'https://account.chsi.com.cn/passport/login'
+        self.i = []
+        # self.post_url = 'https://account.chsi.com.cn/passport/login?service=https%3A%2F%2Fmy.chsi.com.cn%2Farchive%2Fj_spring_cas_security_check'
+        self.logined_url = 'https://my.chsi.com.cn/archive/gdjy/xj/show.action'
+        self.username = input('请输入您的学信网账号')
+        self.password = input('请输入您的学信网密码')
+        self.session = requests.session()
+        self.token()
+
+    #拉取关键字参数
+    def token(self):
+        response = self.session.get(self.login_url,headers=self.headers)
+        response.encoding = 'utf-8'
+        all = []
+        html = pq(response.text)
+        a = html('input').items()
+        for i in a:
+            all.append(i.attr('value'))
+        lt = all[3]
+        execution = all[4]
+        print(response.status_code)
+        self.login(lt,execution)
+
+    #模拟登陆
+    def login(self,lt,execution):
+        post_data ={
+            'username':self.username,
+            'password':self.password,
+            'lt':lt,
+            'execution':execution,
+            '_eventId': 'submit',
+            'submit': '登  录'
+        }
+        try:
+            response = self.session.post(self.login_url,data=post_data,headers=self.headers)
+            response.encoding = 'utf-8'
+            # cookie = self.session.cookies.get_dict()
+            self.info()
+        except Exception:
+            print('请输入正确用户名密码!')
+
+    #数据拉取
+    def info(self):
+        response = self.session.get('https://my.chsi.com.cn/archive/gdjy/xj/show.action',params=self.prarms,headers=self.header)
+        response.encoding = 'utf-8'
+        doc = pq(response.text)
+        self.data(doc)
+
+    #数据清洗
+    def data(self,doc):
+        data_img = doc('.xjxx-img').attr('src')
+        user_img = doc('.pic>img').attr('src')
+        user_img = 'https://my.chsi.com.cn/' + user_img
+        print(data_img,user_img)
+        self.save_img(data_img,user_img)
+
+    #图片存储
+    def save_img(self,data_img,user_img):
+        data = self.session.get(data_img,headers=self.header)
+        with open('data_info'+'.jpg', 'wb') as f:
+            f.write(data.content)
+
+        user = self.session.get(user_img, headers=self.header)
+        with open('user_info'+'.jpg', 'wb') as f:
+            f.write(user.content)
+        self.get_file_content()
+
+    #图片识别
+    def get_file_content(self):
+        with open('data_info.jpg', 'rb') as fp:
+            a = fp.read()
+        client = AipOcr(self.APP_ID, self.API_KEY, self.SECRET_KEY)
+        message = client.basicAccurate(a)  # 返回识别结果
+        res = message['words_result']
+        for s in range(18):
+            self.i.append(res[s]['words'])
+        print(self.i)
+        self.img_text()
+
+
+    #图片处理
+    def img_text(self):
+        square = Image.open("user_info.jpg")
+        square1 = square.filter(ImageFilter.CONTOUR)
+        square1.save("user_info.jpg")
+
+
+if __name__ == '__main__':
+   Login()
